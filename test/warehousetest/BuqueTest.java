1c7bc9d08ab43015cbfd41af5664bfe20e19d8de
Nicolas <nicolas.ale.girotti@gmail.com>
2025-11-10 17:02:22 -0300

    Probando repositorio

--- test/warehousetest/BuqueTest.java
+++ test/warehousetest/BuqueTest.java
@@ -1,6 +1,7 @@
 package warehousetest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
