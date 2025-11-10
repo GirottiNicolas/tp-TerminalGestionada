6d25643d14dcc1d213e6d60750e0aced8afc7224
Perez-JuanCruz <perezjuanchi2004@gmail.com>
2025-11-10 11:46:10 -0300

    Test TerminalGestionada

---
+++ test/terminaltest/TerminalGestionadaTest.java
@@ -0,0 +1,25 @@
+package terminaltest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import terminalgestionada.FachadaTerminal;
+import terminalgestionada.TerminalGestionada;
+
+public class TerminalGestionadaTest {
+	
+	TerminalGestionada terminal;
+	
+	@BeforeEach
+	public void setUp() {
+		terminal = new TerminalGestionada();
+	}
+	
+	@Test
+	public void test1() {
+		assertTrue(false);
+	}
+	
+	
+}
