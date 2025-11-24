e998ceeef71ebd203ac39d0069d281b4b7f43bb0
Nicolas <nicolas.ale.girotti@gmail.com>
2025-11-23 23:53:21 -0300

    Orden como clase abstracta, sin implementacion de interface

--- src/gestion/interfaces/OrdenDeComercio.java
+++
@@ -1,9 +0,0 @@
-package gestion.interfaces;
-
-import java.time.LocalDateTime;
-
-
-
-public interface OrdenDeComercio extends OrdenI,TurnoI {
-	public void setFechaDeNotificacion(LocalDateTime fechaNotificacion);
-}
