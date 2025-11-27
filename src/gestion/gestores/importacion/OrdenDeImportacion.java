e9e9def277405880714a5fe9273a6c18ec6dab35
Nicolas <nicolas.ale.girotti@gmail.com>
2025-11-27 13:26:43 -0300

    Correccion final de Orden

--- src/gestion/gestores/importacion/OrdenDeImportacion.java
+++ src/gestion/gestores/importacion/OrdenDeImportacion.java
@@ -12,13 +12,12 @@ import warehouse.Carga;
 
 public class OrdenDeImportacion extends Orden {
 	
-	LocalDateTime turno;
 	
 	LocalDateTime fechaDeRetiro;
 	
 	public OrdenDeImportacion(Viaje viaje,Carga carga, Camion camion, Cliente cliente,LocalDateTime turno) {
 		super(viaje, carga, camion,cliente);
-		this.turno = turno;
+		this.asignarTurno(turno);
 	}
 
 
